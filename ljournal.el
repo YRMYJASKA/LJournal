64a11257024d30f76a8628777b67ee47d692bffe
Jyry Hjelt <hjelt.jyry@gmail.com>
2022-04-11 22:57:38 +0100

    init

---
+++ ljournal.el
@@ -0,0 +1,35 @@
+;; LJournal --- Date-tree oriented LaTex projects
+;; Copyright (C) 2022  Jyry "Yrmyjaska" Hjelt
+
+;; Author: Jyry Hjelt <jh2821@ic.ac.uk>
+;; Keywords: lisp,latex
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Inspired by blog post: https://castel.dev/post/research-workflow/ .
+;; Aims to create a usable interface to create neatly organised LaTeX
+;; projects with multiple files and a neat date-tree file structure.
+
+;;; Code:
+
+;; Configuration options
+
+
+;; Creating files
+
+(provide 'ljournal)
+;;; ljournal.el ends here
